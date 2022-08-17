5629ac2535c4e8f6f831fe2863f3c31cf346db5d
Le Pham <phamtle724@gmail.com>
2022-08-17 16:34:25 -0700

    did the routes

--- itemsRoutes.js
+++ itemsRoutes.js
@@ -39,11 +39,42 @@ router.get("/:name", function (req, res) {
   return res.json(nameItem);
 });
 
+/** PATCH /items/:name accepts JSON body, modify them, return it */
+router.patch("/:name", function (req, res) {
+  let updateItem;
+
+  for (let item of items.items) {
+    if (item.name === req.params.name) {
+      item.name = req.body.name;
+      item.price = req.body.price;
+      updateItem = item;
+    }
+  }
+
+  if (!updateItem) {
+    throw new NotFoundError("No such item found!");
+  }
+
+  return res
+          .status(200)
+          .json({"updated": updateItem})
+})
 
 
 /** DELETE /items/[id]: delete user, return {message: Deleted} */
-router.delete("/:id", function (req, res) {
-  db.User.delete(req.params.id);
+router.delete("/:name", function (req, res) {
+  let deleteItem;
+  for (let i = 0; i < items.items.length; i++) {
+    if (items.items[i].name === req.params.name) {
+      deleteItem = items.items[i];
+      items.items.splice(i, 1);
+    }
+  }
+
+  if (!deleteItem) {
+    throw new NotFoundError("No such item found!");
+  }
+
   return res.json({ message: "Deleted" });
 });
 
